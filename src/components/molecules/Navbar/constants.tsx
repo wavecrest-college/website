e72dc7053c31749b49fa5265f02302706230c2e1
Akhigbepeace <peaceakhigbe18@gmail.com>
2025-05-04 21:26:24 +0100

    feat: updates apply now link on navbar

--- src/components/molecules/Navbar/constants.tsx
+++ src/components/molecules/Navbar/constants.tsx
@@ -148,7 +148,7 @@ export const sideLinks = [
   },
   {
     name: "APPLY NOW",
-    href: "https://tinyurl.com/WCH-Application-Form-24-25",
+    href: process.env.NEXT_PUBLIC_APPLY_NOW_URL as string, 
     variant: "filled" as const,
   },
 ];
