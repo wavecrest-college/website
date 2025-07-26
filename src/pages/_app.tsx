250e42c2816b6ba2d0b5a98326e295540461308e
Akhigbepeace <peaceakhigbe18@gmail.com>
2025-07-26 18:14:17 +0100

    feat: adds analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import { SessionProvider } from "next-auth/react";
-
+import { Analytics } from "@vercel/analytics/next"
 import Seo from "../components/atoms/Seo";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "slick-carousel/slick/slick.css";
@@ -40,6 +40,7 @@ export default function MyApp({ session, Component, pageProps }: any) {
 
   return (
     <>
+    <Analytics/>
       <Seo {...pageProps.seo} />
 
       <ChakraProvider theme={theme}>
